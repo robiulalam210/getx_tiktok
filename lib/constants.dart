70aa8e9912251f6f57664c7d1a2cd10c7632728b
robiulalam210 <robiulstudies@gmail.com>
2022-12-25 17:56:48 +0600

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+
+// getRandomColor() => Colors.primaries[Random().nextInt(Colors.primaries.length)];
+
+getRandomColor() => [
+  Colors.blueAccent,
+  Colors.redAccent,
+  Colors.greenAccent,
+][Random().nextInt(3)];
+
+// COLORS
+const backgroundColor = Colors.black;
+var buttonColor = Colors.red[400];
+const borderColor = Colors.grey;
+//
+// var pageindex = [
+// DisplayVideo_Screen(),
+//  SearchScreen(),
+//  addVideoScreen(),
+//   Text('Coming Soon In New Updates!'),
+//   ProfileScreen(uid: FirebaseAuth.instance.currentUser!.uid,),
+// ];
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:getx_tiktok/view/screens/auth/signup_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+
+        primarySwatch: Colors.blue,
+      ),
+      home: SignUpScreen(),
+    );
+  }
+}
